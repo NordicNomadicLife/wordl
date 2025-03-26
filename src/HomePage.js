65d4289002e8496310ae3c36ab40363ff9bf3cb4
NordicNomadicLife <Pelle.nilsson.93@hotmail.com>
2025-03-26 16:19:53 +0100

    created a homepage to build on

---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./style.scss"; 
+
+export default function HomePage() {
+
+  return (
+    <div className="home-container">
+      <h1>Välkommen till Wordl!</h1>
+    </div>
+  );
+}
