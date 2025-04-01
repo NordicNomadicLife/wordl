4383952c86adac7dec87d046090ea456f21560da
NordicNomadicLife <Pelle.nilsson.93@hotmail.com>
2025-04-01 16:36:23 +0200

    made an funktion to choose how many letters in the word

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Dropdown({ onSelectLength }) {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value);
+    setSelectedOption(value);
+    onSelectLength(value);
+  };
+
+  return (
+    <div className="dropdownMeny">
+      <label>Choose number of letter: </label>
+      <select value={selectedOption} onChange={handleChange}>
+        <option value="">-- Choose --</option>
+        {[...Array(15)].map((_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
